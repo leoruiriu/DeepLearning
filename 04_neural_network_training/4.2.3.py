bed39994c84c0099575616ca8b4c52579faeace9
wagstyle <hmcho0728@gmail.com>
2016-11-25 20:52:40 +0900

    4 ニューラルネットワークの学習

---
+++ 04_neural_network_training/4.2.3.py
@@ -0,0 +1,21 @@
+# ミニバッチ学習
+
+import sys, os
+sys.path.append(os.pardir) # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+from dataset.mnist import load_mnist
+from function.sigmoid import sigmoid
+from function.softmax import softmax
+
+(x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize=True, one_hot_label=True)
+
+print(x_train.shape) # (60000, 784)
+print(t_train.shape) # (60000, 10)
+
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+print(batch_mask) # [53719 10213 10122 30253 19833 54085  5225 21288  1673 28304]
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
