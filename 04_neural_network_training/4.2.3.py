caad320633883170a11e2703dad5c32f11d7a4fb
wagstyle <hmcho0728@gmail.com>
2016-11-25 20:52:58 +0900

    4 ニューラルネットワークの学習

--- 04_neural_network_training/4.2.3.py
+++ 04_neural_network_training/4.2.3.py
@@ -4,8 +4,6 @@ import sys, os
 sys.path.append(os.pardir) # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 from dataset.mnist import load_mnist
-from function.sigmoid import sigmoid
-from function.softmax import softmax
 
 (x_train, t_train), (x_test, t_test) = \
         load_mnist(normalize=True, one_hot_label=True)
